58cf2dd404e2409b1c47ce6108df6e6cb01e7c51
liyiru <liyr@ehualu.com>
2022-05-04 18:45:50 -0400

    update prepared data used in finetune process

--- Plan3/run_finetune.sh
+++ Plan3/run_finetune.sh
@@ -1,4 +1,3 @@
-pip install --upgrade openai
-export OPENAI_API_KEY="<MY_APT_KEYS>"
-openai tools fine_tunes.prepare_data -f "C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\Plan3\data_emet_csv.csv"
-openai api fine_tunes.create -t "C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\Plan3\data_emet_tsv_prepared.jsonl" -m "davinci"
+# pip install --upgrade openai
+export OPENAI_API_KEY=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
+openai tools fine_tunes.prepare_data -f C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\gandalf_ao3.tsv
--- Plan3/text_prep.py
+++ Plan3/text_prep.py
@@ -7,31 +7,30 @@ def process(data_path,filename):
     with open(data_path+filename+txt_str,
               "r",encoding="utf8") as f:
         data = f.readlines()
-        
+
     temp = [x.replace("<\|endoftext\|>","").replace("Emet-Selch:","").replace("<|endoftext|>","").strip() for x in data]
     text_cleaned = tokenize.sent_tokenize(' '.join(temp))
-    output = open(data_path+filename+tsv_str, "w")
+    output = open(data_path+filename+tsv_str, "w", encoding="utf8")
     output.write("prompt\tcompletion\n")
     for item in text_cleaned:
-        output.write("\t{item}\n")
+        output.write(f"\t{item}\n")
     output.close()
 
 def main():
 
     # setups
-    
-    data_path = "C:/Users/yl5865/MLLU_Final_Project/data/"
+    data_path = "C:/Users/fyqaw/Documents/NYU/2022-2023/DS203/finalProject/MLLU_Final_Project/data/"
     data_names = ["emet_ao3",
                   "emet_dialogue",
                   "gandalf_ao3",
                   "gandalf_dialogue",
-                  "hermoine_ao3",
-                  "hermoine_dialogue"]
+                  "hermione_ao3",
+                  "hermione_dialogue"]
 
-    
+
     for file in data_names:
         process(data_path,file)
-    
+
 
 
 if __name__ == '__main__':
