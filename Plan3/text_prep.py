9edaea78e23a7bf75c6ba35da10eac3c083fc208
Yiru Li <yl5865@ad.nyu.edu>
2022-05-04 18:03:03 -0400

    data folder created

--- Plan3/text_prep.py
+++ Plan3/text_prep.py
@@ -1,20 +1,37 @@
 import re
 from nltk import tokenize
 
-def main():
-
-    with open("MLLU_Final_Project\Plan3\dialogue_emet.txt",
-                "r",encoding="utf8") as f:
+def process(data_path,filename):
+    tsv_str = ".tsv"
+    txt_str = ".txt"
+    with open(data_path+filename+txt_str,
+              "r",encoding="utf8") as f:
         data = f.readlines()
-    #pat = r"<\|endoftext\|>Emet-Selch: (.+) <\|endoftext\|>"
-    #temp = [re.search(pat, x).group(0) for x in data]
+        
     temp = [x.replace("<\|endoftext\|>","").replace("Emet-Selch:","").replace("<|endoftext|>","").strip() for x in data]
     text_cleaned = tokenize.sent_tokenize(' '.join(temp))
-    output = open("MLLU_Final_Project\Plan3\data_emet_tsv.tsv", "w")
+    output = open(data_path+filename+tsv_str, "w")
     output.write("prompt\tcompletion\n")
     for item in text_cleaned:
-        output.write(f"\t{item}\n")
+        output.write("\t{item}\n")
     output.close()
+
+def main():
+
+    # setups
+    
+    data_path = "C:/Users/yl5865/MLLU_Final_Project/data/"
+    data_names = ["emet_ao3",
+                  "emet_dialogue",
+                  "gandalf_ao3",
+                  "gandalf_dialogue",
+                  "hermoine_ao3",
+                  "hermoine_dialogue"]
+
+    
+    for file in data_names:
+        process(data_path,file)
+    
 
 
 if __name__ == '__main__':
