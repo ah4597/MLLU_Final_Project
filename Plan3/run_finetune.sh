9bc773c7fd16328d30ede89a7af4f8d936d3f5e2
liyiru <liyr@ehualu.com>
2022-05-05 23:57:06 -0400

    added combined data, upload result generation jpnotebook

--- Plan3/run_finetune.sh
+++ Plan3/run_finetune.sh
@@ -1,7 +1,10 @@
 # pip install --upgrade openai
-# set OPENAI_API_KEY=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
+# set OPENAI_API_KEY=my_api_key
 # openai tools fine_tunes.prepare_data -f C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\gandalf_ao3.tsv
-openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_ao3_prepared.jsonl -m davinci
-openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_ao3_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_ao3_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_ao3_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\gandalf_dialogue_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_dialogue_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_combined_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_combined_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\gandalf_combined_prepared.jsonl -m davinci
