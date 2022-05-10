b7c59a4be9a29d4f97cb6b9f85adb3943ad349dd
Alex <ah4597@nyu.edu>
2022-05-09 22:49:59 -0400

    limited data to match dialogue_emet's, added new data folder for ease of access

--- scraper/hp/process_data.py
+++ scraper/hp/process_data.py
@@ -1,11 +1,15 @@
 import csv
+PRINT_LIMIT = 189
 
-dialogue_hermione = open('dialogue_hermione.txt', 'w', encoding='utf-8')
-
+dialogue_hermione = open('../data/dialogue_hermione.jsonl', 'w', encoding='utf-8')
+num_lines_printed = 0
 with open('Dialogue.csv', newline='', encoding='iso-8859-1') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row['Character ID'] == '3':
-            dialogue_hermione.write(f'<|endoftext|>{row["Dialogue"]}<|endoftext|>\n')
+            dialogue_hermione.write(f'{{"prompt":"", "completion":"{row["Dialogue"].strip()}"}}\n')
+            num_lines_printed += 1
+            if num_lines_printed >= PRINT_LIMIT:
+                break
 
 dialogue_hermione.close()
--- scraper/lotr/process_texts.py
+++ scraper/lotr/process_texts.py
@@ -1,19 +1,22 @@
 from nltk import tokenize
-
+PRINT_LIMIT = 189
 texts = ['The_Fellowship_Of_The_Ring.txt',
         'The_Two_Towers.txt',
         'The_Return_Of_The_King.txt']
 
-mentions_gandalf = open('gandalf.txt', 'w', encoding='utf-8')
+mentions_gandalf = open('../data/gandalf.jsonl', 'w', encoding='utf-8')
+num_lines_printed = 0
 for text in texts:
-    print(f'Processing {text}...')
-    read_text = open(f'./texts/{text}', 'r', encoding='iso-8859-1').read()
-    tokens = tokenize.sent_tokenize(read_text)
+    if num_lines_printed < PRINT_LIMIT + 5:
+        print(f'Processing {text}...')
+        read_text = open(f'./texts/{text}', 'r', encoding='iso-8859-1').read()
+        tokens = tokenize.sent_tokenize(read_text)
 
-    for token in tokens:
-        if 'Gandalf' in token:
-            mentions_gandalf.write(f'<|endoftext|>{token}<|endoftext|>\n')
-
-    mentions_gandalf.write('\n')
+        for token in tokens:
+            if ('Gandalf' in token) and (len(token.split()) > 10):
+                mentions_gandalf.write(f'{{"prompt":"", "completion":"{token.strip()}"}}\n')
+                num_lines_printed += 1
+                if num_lines_printed >= PRINT_LIMIT + 5:
+                    break
 
 mentions_gandalf.close()
