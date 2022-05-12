a14e81e28e6c13a495d898a5a897549b4da365b6
liyiru <liyr@ehualu.com>
2022-05-12 05:49:26 -0400

    all done, final version of pre-coding, only result analysis should be pushed after this commit

--- Plan3/run_finetune.sh
+++ Plan3/run_finetune.sh
@@ -1,10 +1,10 @@
 # pip install --upgrade openai
 # set OPENAI_API_KEY=my_api_key
-# openai tools fine_tunes.prepare_data -f C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\gandalf_ao3.tsv
+# openai tools fine_tunes.prepare_data -f C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\emet_ao3_v2.tsv
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_ao3_prepared.jsonl -m davinci
-# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_ao3_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_ao3_v2_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\gandalf_dialogue_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_dialogue_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\hermione_combined_prepared.jsonl -m davinci
-# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_combined_prepared.jsonl -m davinci
+# openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\emet_combined_v2_prepared.jsonl -m davinci
 # openai api fine_tunes.create -t C:\Users\fyqaw\Documents\NYU\2022-2023\DS203\finalProject\MLLU_Final_Project\data\Prepared\gandalf_combined_prepared.jsonl -m davinci
